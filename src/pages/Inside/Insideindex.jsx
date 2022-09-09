b3270d5844f05b55c4269d663708fe3ac6892c6f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-09 00:39:39 -0300

    :construction: build: index que para acesso restr.

---
+++ src/pages/Inside/Insideindex.jsx
@@ -0,0 +1,27 @@
+import axios from "axios"
+import { useContext, useEffect, useState } from "react"
+import AuthContext from "../../Context/AutenticaçãoContext"
+
+export default function Insideindex() {
+
+    const { iduserglobal } = useContext(AuthContext)
+
+    const [writers, setWriters] = useState()
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/users/writers/${iduserglobal}`)
+        .then((result) => setWriters(result.data))
+    }, [])
+
+    return (
+        <>
+            <div>Ola usuario de id {iduserglobal}</div>
+            {writers?.map(resp => (
+                <li key={resp.id}>
+                    <div>{ resp.name }</div>
+                </li>
+            ))}
+        </>
+    )
+    
+}
