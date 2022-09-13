24b0ebb193e121aa74a9e56310cbe9c857eca159
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-12 23:22:24 -0300

    :lipstick: style: estiliza Componente de livros

--- src/Components/BookComponent.jsx
+++ src/Components/BookComponent.jsx
@@ -3,12 +3,14 @@ import TrataWriter from '../functions/TrataWriter'
 export default function BookComponent(props) {
     const resp = props.resp
     return (
-        <div>
-            <img src={resp.cover} />
-            <div>{resp.title}</div>
-            <div>{TrataWriter(resp.writer_id)}</div>
-            <button>Update</button>
-            <button>Delete</button>
+        <div className={`oi`}>
+            <img className={`h-60 w-40 rounded-3xl mb-4`} src={resp.cover} />
+            <div className={`font-semibold text-base w-[180px] mb-2`}>{resp.title}</div>
+            <div className={`text-xs mb-4`}>{TrataWriter(resp.writer_id)}</div>
+            <div className={`flex flex-row justify-center content-center w-40`}>
+                <button className={`py-[2px] px-2 mx-1 rounded-full bg-mainColor`}>Update</button>
+                <button className={`py-[2px] px-2 mx-1 rounded-full bg-deleteColor`}>Delete</button>
+            </div>
         </div>
     )
 }
