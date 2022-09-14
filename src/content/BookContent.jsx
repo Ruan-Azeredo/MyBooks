c9a2ead35f6e7623a240eb5acace62d2061b0c90
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-13 23:33:08 -0300

    :lipstick: style: centraliza os livros

--- src/content/BookContent.jsx
+++ src/content/BookContent.jsx
@@ -1,7 +1,11 @@
 export default function (props) {
     return (
-        <div className={`
-            grid grid-cols-4 gap-4
-        `}>{props.children}</div>
+        <div className={`w-full flex`}>
+            <div className={`
+                grid grid-cols-4 gap-4 w-4/6 m-auto
+            `}>
+                {props.children}
+            </div>
+        </div>
     )
 }
