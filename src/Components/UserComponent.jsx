be389ba77a3575e2b87d61f1db41c39685e0ece6
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-15 00:45:25 -0300

    :sparkles: feat: card de usuario

--- src/Components/UserComponent.jsx
+++ src/Components/UserComponent.jsx
@@ -3,13 +3,16 @@ import AuthContext from "../Context/AutenticaçãoContext"
 
 export default function UserComponent() {
     const { photoglobal } = useContext(AuthContext)
-    console.log(photoglobal)
+    const nomeCompleto = 'Ruan Azeredo dos Santos Gomes'
+    const nome = nomeCompleto.split(" ")[0] //Pega só o primeiro nome
     return (
         <div className={`
         w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px]
-        bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100     
+        bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100 grid   
         `}>
             <img src={photoglobal} className={`w-20 h-20 rounded-3xl mx-auto mt-8`} />
+            <div className={`mx-10 px-2`}>Bem vindo de volta</div>
+            <div className={`font-bold justify-self-center text-xl pb-4`}>{ nome }</div>
         </div>
     )
 }
