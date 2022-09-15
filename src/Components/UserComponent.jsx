9bafd95289409d56e308da41a11681bc0b0f6a32
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-14 23:45:24 -0300

    :construction: build: componente de usuario

---
+++ src/Components/UserComponent.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+import AuthContext from "../Context/AutenticaçãoContext"
+
+export default function UserComponent() {
+    const { photoglobal } = useContext(AuthContext)
+    console.log(photoglobal)
+    return (
+        <div className={`
+        w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px]
+        bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100     
+        `}>
+            <img src={photoglobal} alt="" />
+        </div>
+    )
+}
