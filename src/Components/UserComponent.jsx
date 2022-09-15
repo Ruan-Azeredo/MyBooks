dfd20100de53802c62701fa89f2dddd40fdfdfbc
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-14 23:58:19 -0300

    :construction: build: adiciona foto dentro de user

--- src/Components/UserComponent.jsx
+++ src/Components/UserComponent.jsx
@@ -9,7 +9,7 @@ export default function UserComponent() {
         w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px]
         bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100     
         `}>
-            <img src={photoglobal} alt="" />
+            <img src={photoglobal} className={`w-20 h-20 rounded-3xl mx-auto mt-8`} />
         </div>
     )
 }
