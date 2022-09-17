a1ff069332e72eff3da9f0e3d8574cae3fbf8254
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 11:26:38 -0300

    :construction: build: função trata book_id->cover

---
+++ src/functions/TrataBook.js
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import BooksContext from "../Context/BooksContext"
+
+const TrataBook = (book_id, y = 0, respss = null) => {
+
+    const { booksglobal } = useContext(BooksContext)
+
+    const books = booksglobal
+
+    if (books[y].id == book_id) {
+        respss = books[y].cover
+        return respss
+    }
+    
+    if (books[y+1] != null) {
+        y = y +1
+        return TrataBook(book_id, y, respss)
+    }
+}
+
+export default TrataBook
