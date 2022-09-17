9e8e296b7854abeb313f5c4dd677fcbd423ceeaf
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-16 23:41:17 -0300

    :construction: build: content review

---
+++ src/content/ReviewContentIndex.jsx
@@ -0,0 +1,7 @@
+export default function ReviewContentIndex(props) {
+    return (
+        <div className="flex flex-row bg-red-100">           
+            {props.children}
+        </div>
+    )
+}
