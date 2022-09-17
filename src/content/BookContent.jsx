33570902af3321b2515fffe5d9825973ea00fe9f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 13:03:23 -0300

    :sparkles: feat: adiciona 'Livros'

--- src/content/BookContent.jsx
+++ src/content/BookContent.jsx
@@ -1,10 +1,12 @@
 export default function (props) {
     return (
-        <div className={`w-full flex col-start-2 col-end-6`}>
+        <div className={`w-full flex col-start-2 col-end-6 mt-6`}>
+            <div className={`w-full text-xl font-bold`}>Livros
             <div className={`
-                grid grid-cols-4 w-full
+                grid grid-cols-4 w-full mt-4 font-medium
             `}>
                 {props.children}
+            </div>
             </div>
         </div>
     )
