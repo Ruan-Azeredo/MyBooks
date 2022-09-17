056965c2f98e2cdfbc0115e2db013a67e092a184
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 11:34:42 -0300

    :lipstick: style: distancia text e data review

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -24,7 +24,7 @@ export default function ReviewsComponentIndex(props) {
         <div className={`bg-mainColor h-32 w-96 mr-16 relative rounded-3xl`}>
             <img className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
-            <div className={`left-[150px] relative w-56`}>{limitText(resp)}</div>
+            <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp)}</div>
         </div>
     )
 }
