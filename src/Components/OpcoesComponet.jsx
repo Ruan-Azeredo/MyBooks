aa8c89623b722d5adbe1024f236467194961e10a
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 13:40:18 -0300

    :construction: build: opoções sidebar component

---
+++ src/Components/OpcoesComponet.jsx
@@ -0,0 +1,5 @@
+export default function OpcoesComponent(props) {
+    return (
+        <div className={`font-semibold w-16 text-center mx-auto mt-[34px] hover:text-yellow-400`}>{props.value}</div>
+    )
+}
