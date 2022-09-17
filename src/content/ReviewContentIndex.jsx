3f15acdda318737a417c165535444ea5bf236a5b
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 13:04:16 -0300

    :lipstick: style: espaçamento top reviews index

--- src/content/ReviewContentIndex.jsx
+++ src/content/ReviewContentIndex.jsx
@@ -1,6 +1,6 @@
 export default function ReviewContentIndex(props) {
     return (
-        <div className="flex flex-row bg-red-100">           
+        <div className="flex flex-row mt-28">           
             {props.children}
         </div>
     )
