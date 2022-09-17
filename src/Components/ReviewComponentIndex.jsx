d2e710b19a169b8821f0ee3130b25fec36b5d68c
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 11:24:46 -0300

    :sparkles: feat: função limita tamanho do texto

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -10,11 +10,21 @@ export default function ReviewsComponentIndex(props) {
         return dia + '/' + mes + '/' + ano
     }
     
+    const limitText = (resp) => {
+        if (resp.text.length > 80) {
+            return resp.text.slice(0, 80) + '...'
+        } else {
+            return resp.text
+        }
+    }
+
+    console.log(resp.text.length)
+
     return (
         <div className={`bg-mainColor h-32 w-96 mr-16 relative rounded-3xl`}>
-            <img className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)} />
+            <img className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
-            <div className={`left-[150px] relative w-56`}>{ resp.text }</div>
+            <div className={`left-[150px] relative w-56`}>{limitText(resp)}</div>
         </div>
     )
 }
