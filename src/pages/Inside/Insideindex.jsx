93456a3b0975ce32fbc5298e674a6280916ab56a
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-17 11:27:42 -0300

    :construction: feat: get reviews insideindex

--- src/pages/Inside/Insideindex.jsx
+++ src/pages/Inside/Insideindex.jsx
@@ -1,16 +1,27 @@
 import axios from "axios"
 import Router from "next/router"
 import { useContext, useEffect, useState } from "react"
+import BookComponent from "../../components/BookComponent"
+import ReviewsComponentIndex from "../../components/ReviewComponentIndex"
+import SeeMoreComponent from "../../components/SeeMoreComponent"
+import SidebarComponent from "../../components/SidebarComponent"
+import UserComponent from "../../components/UserComponent"
+import BookContent from "../../content/BookContent"
+import ReviewContentIndex from "../../content/ReviewContentIndex"
 import AuthContext from "../../Context/AutenticaçãoContext"
-import TrataWriter from "../../functions/TrataWriter"
+import BooksContext from "../../Context/BooksContext"
+import WritersContext from "../../Context/WritersContext"
 
 export default function Insideindex() {
 
-    // const { iduserglobal } = useContext(AuthContext)
+    // const { iduserglobal, photoglobal } = useContext(AuthContext)
     const iduserglobal = 2
+    const { setWritersglobal } = useContext(WritersContext)
+    const { booksglobal, setBooksglobal } = useContext(BooksContext)
 
     const [books, setBooks] = useState()
     const [writers, setWriters] = useState()
+    const [reviews, setReviews] = useState()
 
 
     useEffect(() => {
@@ -18,24 +29,45 @@ export default function Insideindex() {
             Router.replace('/')
         } else {
             axios.get(`http://localhost:3001/users/books/${iduserglobal}`)
-            .then((result) => setBooks(result.data))
+            .then((result) => setBooksglobal(result.data))
             
             axios.get(`http://localhost:3001/users/writers/${iduserglobal}`)
-            .then((result) => setWriters(result.data))
+            .then((result) => setWritersglobal(result.data))
+            
+            axios.get(`http://localhost:3001/users/reviews/${iduserglobal}`)
+            .then((result) => setReviews(result.data))
         }
     }, [])
 
+    const renderizaReview = (resp) => {
+        if(resp.id <= 2){
+            return (
+                <div key={resp.id} className={``}>
+                    <ReviewsComponentIndex resp={resp} />
+                </div>
+            )
+        }
+    }
+
     return (
-        <>
+        <div className={`grid grid-cols-6`}>
             <div>Ola usuario de id {iduserglobal}</div>
-            {books?.map(resp => (
-                <li key={resp.id}>
-                    <div>{ resp.cover }</div>
-                    <div>{resp.title}</div>
-                    <div>{TrataWriter(resp.writer_id, writers)}</div>
-                </li>
-            ))}
-        </>
+            <UserComponent />
+            <SidebarComponent />
+            <ReviewContentIndex>
+                {reviews?.map(resp => (
+                    renderizaReview(resp)
+                ))}
+                <SeeMoreComponent/>
+            </ReviewContentIndex>
+            <BookContent>
+                {booksglobal?.map(resp => (
+                    <div key={resp.id}>    
+                        <BookComponent resp={resp} />
+                    </div>
+                ))}
+            </BookContent>
+        </div>
     )
     
 }
