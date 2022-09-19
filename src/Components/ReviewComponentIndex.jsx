b9754240c9337c96ca871d9f4e31204b9759a5cd
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-18 23:29:17 -0300

    :fire: fix: tira console.log

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -18,8 +18,6 @@ export default function ReviewsComponentIndex(props) {
         }
     }
 
-    console.log(resp.text.length)
-
     return (
         <div className={`bg-mainColor h-32 w-96 mr-16 relative rounded-3xl`}>
             <img className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
