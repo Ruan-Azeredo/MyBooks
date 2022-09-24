45fc50858faaa0fce818e8538be91695179fd4cb
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-23 23:56:27 -0300

    :recycle: refactor: chama função de limitText

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -1,3 +1,4 @@
+import limitText from "../functions/limitText"
 import TrataBook from "../functions/TrataBook"
 
 export default function ReviewsComponentIndex(props) {
@@ -9,20 +10,12 @@ export default function ReviewsComponentIndex(props) {
         const ano = resp.createdAt[2] + resp.createdAt[3]
         return dia + '/' + mes + '/' + ano
     }
-    
-    const limitText = (resp) => {
-        if (resp.text.length > 80) {
-            return resp.text.slice(0, 80) + '...'
-        } else {
-            return resp.text
-        }
-    }
 
     return (
         <div className={`bg-mainColor h-32 w-96 mr-16 relative rounded-3xl`}>
             <img className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
-            <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp)}</div>
+            <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp.text, 80)}</div>
         </div>
     )
 }
