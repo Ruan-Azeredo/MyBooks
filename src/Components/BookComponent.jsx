2652909e1ae64d43df15ee887c0ddbbc37afd43d
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-24 15:17:06 -0300

    construction: build: adiciona + Resenhas

--- src/Components/BookComponent.jsx
+++ src/Components/BookComponent.jsx
@@ -6,7 +6,10 @@ export default function BookComponent(props) {
 
     return (
         <div className={`mb-4`}>
-            <img className={`h-60 w-40 rounded-3xl mb-4`} src={resp.cover} />
+            <div className={`absolute pt-24 px-8 -z-20`}>+ Resenha</div>
+            <button>
+                <img className={`h-60 w-40 rounded-3xl mb-4 hover:opacity-20 z-30`} src={resp.cover}/>
+            </button>
             <div className={`h-[90px]`}>
                 <div className={`font-semibold text-base w-[180px] mb-2`}>{limitText(resp.title, 40)}</div>
                 <div className={`text-xs mb-4`}>{TrataWriter(resp.writer_id)}</div>
