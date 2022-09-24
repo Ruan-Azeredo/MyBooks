9e77dae54699f591a8854b43027053db945c0d7f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-24 00:28:03 -0300

    :lipstick: feat: estiliza botão DarkMode

--- src/Components/ButtonDarkModeComponent.jsx
+++ src/Components/ButtonDarkModeComponent.jsx
@@ -7,17 +7,36 @@ export default function ButtonDarkModeComponent() {
 
     return (
         <>
-            <div className={`w-[70px] bg-gray-600 rounded-full group`}>
+            {/* <div className={`w-[70px] bg-gray-600 rounded-full group`}> */}
 
-                <input type='checkbox' className={`
+                {/* <input onClick={alterarTema} type='checkbox' className={`
                 w-[70px] h-6 font-normal text-w peer relative flex decoration-transparent bg-transparent opacity-0`} />
 
-                <div className={`ml-1 w-[22px] h-[22px] bg-white rounded-full peer-checked:translate-x-10
-                absolute top-[19px]`}></div>
+                {tema == 'light' ? (
 
-                <button className="ml-20 bg-gray-300" onClick={alterarTema}>{tema}</button>
+                    <div className={`ml-1 w-[22px] h-[22px] bg-white rounded-full peer-checked:translate-x-10
+                    absolute top-[19px]`}></div>
+                    
+                ) : (
+                        
+                    <div className={`ml-1 w-[22px] h-[22px] bg-amber-300 rounded-full peer-checked:translate-x-10
+                    absolute top-[19px]`}></div> 
+                        
+                )} */}
 
-            </div>
+                {tema == 'light' ? (
+                    <button className={`bg-gray-600 rounded-full w-[80px] h-7 flex flex-row pt-[3px]`} onClick={alterarTema}>
+                    <div className={`ml-1 w-[22px] h-[22px] bg-white rounded-full`}></div>
+                    <div className={`ml-[5px] text-white`}>Dark</div>
+                    </button>
+                ) : (
+                    <button className={`bg-gray-200 rounded-full w-[80px] h-7 flex flex-row pt-[3px]`} onClick={alterarTema}>
+                        <div className={`ml-3 text-amber-400`}>Light</div>
+                        <div className={`ml-2 w-[22px] h-[22px] bg-amber-400 rounded-full`}></div>
+                    </button>
+                )}
+
+            {/* </div> */}
         </>
     )
 }
