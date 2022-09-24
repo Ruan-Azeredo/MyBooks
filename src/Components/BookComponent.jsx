33d9e9af7a5d9ce51d195708b5cfb0b8721024b4
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-23 23:55:46 -0300

    :sparkles: feat: limita tamanho titulos livros

--- src/Components/BookComponent.jsx
+++ src/Components/BookComponent.jsx
@@ -1,12 +1,16 @@
+import limitText from '../functions/limitText'
 import TrataWriter from '../functions/TrataWriter'
 
 export default function BookComponent(props) {
     const resp = props.resp
+
     return (
-        <div className={`oi`}>
+        <div className={`mb-4`}>
             <img className={`h-60 w-40 rounded-3xl mb-4`} src={resp.cover} />
-            <div className={`font-semibold text-base w-[180px] mb-2`}>{resp.title}</div>
-            <div className={`text-xs mb-4`}>{TrataWriter(resp.writer_id)}</div>
+            <div className={`h-[90px]`}>
+                <div className={`font-semibold text-base w-[180px] mb-2`}>{limitText(resp.title, 40)}</div>
+                <div className={`text-xs mb-4`}>{TrataWriter(resp.writer_id)}</div>
+            </div>
             <div className={`flex flex-row justify-center content-center w-40`}>
                 <button className={`py-[2px] px-2 mx-1 rounded-full font-normal text-base bg-mainColor`}>Update</button>
                 <button className={`py-[2px] px-2 mx-1 rounded-full font-normal text-base bg-deleteColor`}>Delete</button>
