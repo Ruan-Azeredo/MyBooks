188495dbfc77f0164c2dffd32c53167b12199a53
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-25 15:07:56 -0300

    :construction: build: fixa sidebar de opções

--- src/Components/SidebarComponent.jsx
+++ src/Components/SidebarComponent.jsx
@@ -2,9 +2,9 @@ import OptionsComponent from "./OptionsComponent";
 
 export default function SidebarComponent() {
     return (
-        <div className={`w-full flex col-start-1 col-end-2 h-[400px]`}>
+        <div className={`w-full flex col-start-1 col-end-2 fixed left-8 top-[310px]`}>
             <div className={`
-                w-40 h-80 rounded-3xl m-auto
+                w-40 h-80 rounded-3xl
                 bg-gradient-to-br from-neutral-200 via-neutral-50 to-neutral-50 sombra
                 
             `}>
