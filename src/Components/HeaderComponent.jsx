6ff78a5ed90ba5a294592600df93712de81d6d57
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-25 15:06:13 -0300

    :sparkles: feat: cria bg transparente header

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -2,7 +2,7 @@ import ButtonDarkModeComponent from "./ButtonDarkModeComponent";
 
 export default function HeaderComponent() {
     return (
-        <div className={`mt-4 flex`}>
+        <div className={`py-2 col-start-1 col-end-7 flex bg-brancoTransparencia backdrop-blur-sm z-30 fixed top-0`}>
             <div className={`ml-[200px]`}>
                 <div className={`ml-2 font-semibold`}>MyBooks.com</div>
                 <div className={`ml-[4px] h-1 w-[110px] -top-[6px] -z-30 relative bg-yellow-300 dark:bg-blue-600`}></div>
