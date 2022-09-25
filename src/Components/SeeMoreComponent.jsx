818dde5618066cbb3bd71c8cdb8da0fc8061f357
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-25 15:07:23 -0300

    :bug: fix: ajeita seeMoreComponent

--- src/Components/SeeMoreComponent.jsx
+++ src/Components/SeeMoreComponent.jsx
@@ -1,7 +1,7 @@
 export default function SeeMoreComponent() {
     return (
         <div>
-            <div className={`bg-yellow-100 h-32 w-56 relative rounded-l-3xl`}>
+            <div className={`bg-yellow-100 h-32 w-[228px] relative rounded-l-3xl`}>
                 <div className={`
                 h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4 bg-yellow-100
                 `}></div>
