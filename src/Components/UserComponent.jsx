a2994632ec0d9681546b5fdfe6e3175de245c80d
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-29 09:54:22 -0300

    :recycle: refactor: volta tag imgs

--- src/Components/UserComponent.jsx
+++ src/Components/UserComponent.jsx
@@ -12,9 +12,9 @@ export default function UserComponent() {
         w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px] sticky top-11
         bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100 grid   
         `}>
-            <Image alt="" src={photoglobal} width='80px' height='80px' className={`rounded-3xl mx-auto mt-8`} />
+            <img alt="" src={photoglobal} className={`h-20 w-20 rounded-3xl mx-auto mt-8`} />
             <div className={`mx-10 px-2`}>Bem vindo de volta</div>
-            <div className={`font-bold justify-self-center text-xl pb-4`}>{ nameglobal }</div>
+            <div className={`font-bold justify-self-center text-xl pb-4`}>{ nome }</div>
         </div>
     )
 }
