334d8202cf4e329b25f2fc4b787c1a050666bbd1
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 12:48:45 -0300

    :recycle: refactor: refaz sideBar mais simples

--- src/Components/OptionsComponent.jsx
+++
@@ -1,5 +0,0 @@
-export default function OptionsComponent(props) {
-    return (
-        <div className={`font-semibold w-16 text-center mx-auto mt-[34px] hover:text-yellow-400`}>{props.value}</div>
-    )
-}
--- src/Components/SidebarComponent.jsx
+++ src/Components/SidebarComponent.jsx
@@ -4,14 +4,15 @@ export default function SidebarComponent() {
     return (
         <div className={`w-full flex col-start-1 col-end-2 fixed left-8 top-[310px]`}>
             <div className={`
-                w-40 h-80 rounded-3xl
+                w-40 h-80 rounded-3xl grid
                 bg-gradient-to-br from-neutral-200 via-neutral-50 to-neutral-50 sombra
+                font-semibold flex-col text-center justify-items-center py-4
                 
             `}>
-                <OptionsComponent value='+ Autor'></OptionsComponent>
-                <OptionsComponent value='+ Livro'></OptionsComponent>
-                <OptionsComponent value='Resenhas & Autores'></OptionsComponent>
-                <OptionsComponent value='Logout'></OptionsComponent>
+                <button className="w-16 hover:text-yellow-400">+ Autor</button>
+                <button className="w-16 hover:text-yellow-400">+ Livro</button>
+                <button className="w-16 hover:text-yellow-400">Resenhas & Autores</button>
+                <button className="w-16 hover:text-yellow-400">Logout</button>
             </div>
         </div>
     )
