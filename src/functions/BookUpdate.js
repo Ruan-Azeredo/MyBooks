76fdc5c317541723790000aabdb396f8d23ef0a7
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-29 23:56:36 -0300

    :recycle: refactor: muda logica do BookUpdate

--- src/functions/BookUpdate.js
+++ src/functions/BookUpdate.js
@@ -1,8 +1,23 @@
+import { useContext } from 'react'
+import AuthContext from '../Context/AutenticaçãoContext'
+
 const axios = require('axios')
 
-export default function BookUpdate(id, titlevalue, covervalue){
-    axios.put(`http://localhost:3001/users/books/1/1/${id}`, {
-        title: titlevalue,
-        cover: covervalue
-    })
+export default function BookUpdate(id, titlevalue, writervalue, covervalue, createdAt) {
+
+    // const {iduserglobal} = useContext(AuthContext)
+    const iduserglobal = 2
+    const deleta = (id) => {
+        axios.delete(`http://localhost:3001/users/books/1/1/${id}`)
+    }
+    console.log('writervalue:', writervalue)
+    const posta = (titlevalue, writervalue, covervalue) => {
+        axios.post(`http://localhost:3001/users/books/${iduserglobal}/${writervalue}/`, {
+            title: titlevalue,
+            cover: covervalue,
+            createdAt: createdAt
+        })
+    }
+    deleta(id)
+    posta(titlevalue, writervalue, covervalue)
 }
