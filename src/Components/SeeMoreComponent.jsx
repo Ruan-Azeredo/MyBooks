8c399b8bddeb3c00d0801ec911b2109f77c959db
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 15:43:14 -0300

    :lipstick: style: darkMode

--- src/Components/SeeMoreComponent.jsx
+++ src/Components/SeeMoreComponent.jsx
@@ -1,9 +1,9 @@
 export default function SeeMoreComponent() {
     return (
         <div>
-            <div className={`bg-yellow-100 h-32 w-[228px] relative rounded-l-3xl`}>
+            <div className={`bg-yellow-100 dark:bg-slate-700 h-32 w-[228px] relative rounded-l-3xl`}>
                 <div className={`
-                h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4 bg-yellow-100
+                h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4 bg-yellow-100 dark:bg-slate-700
                 `}></div>
                 <div className={`pt-28 pl-8 absolute -top-16 left-7 font-bold text-xl`}>+ Resenhas</div>
             </div>
