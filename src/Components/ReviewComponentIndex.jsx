480e21c64b789faa610b2b77226c774d1fc5a65e
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 15:43:05 -0300

    :lipstick: style: darkMode

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -13,7 +13,7 @@ export default function ReviewsComponentIndex(props) {
     }
 
     return (
-        <div className={`bg-mainColor h-32 w-96 mr-16 relative rounded-3xl`}>
+        <div className={`bg-mainColor dark:bg-mainDark h-32 w-96 mr-16 relative rounded-3xl`}>
             <img alt="" layout="fill" className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
             <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp.text, 80)}</div>
