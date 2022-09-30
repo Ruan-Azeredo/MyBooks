59d6df4909e3ada426bba0ddf9ca5af507456813
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 15:43:27 -0300

    :lipstick: style: darkMode

--- src/Components/SidebarComponent.jsx
+++ src/Components/SidebarComponent.jsx
@@ -32,14 +32,16 @@ export default function SidebarComponent() {
         <div className={`w-full flex col-start-1 col-end-2 fixed left-8 top-[310px]`}>
             <div className={`
                 w-40 h-80 rounded-3xl grid
-                bg-gradient-to-br from-neutral-200 via-neutral-50 to-neutral-50 sombra
+                bg-gradient-to-br from-neutral-200 via-neutral-50 to-neutral-50
+                dark:bg-gradient-to-br dark:from-neutral-700 dark:via-neutral-700 dark:to-neutral-700
+                sombra
                 font-semibold flex-col text-center justify-items-center py-4
                 
             `}>
-                <button className="w-16 hover:text-yellow-400" onClick={openModal} value='Autor'>+ Autor</button>
-                <button className="w-16 hover:text-yellow-400" onClick={openModal} value='Livro'>+ Livro</button>
-                <button className="w-16 hover:text-yellow-400">Resenhas & Autores</button>
-                <button className="w-16 hover:text-yellow-400">Logout</button>
+                <button className="w-16 hover:text-yellow-400 dark:hover:text-blueDark" onClick={openModal} value='Autor'>+ Autor</button>
+                <button className="w-16 hover:text-yellow-400 dark:hover:text-blueDark" onClick={openModal} value='Livro'>+ Livro</button>
+                <button className="w-16 hover:text-yellow-400 dark:hover:text-blueDark">Resenhas & Autores</button>
+                <button className="w-16 hover:text-yellow-400 dark:hover:text-blueDark">Logout</button>
             </div>
 
             <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
