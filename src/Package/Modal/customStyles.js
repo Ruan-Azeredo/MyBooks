fb66dda9e0ec6764b3c14969db69e583b52430ba
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 23:55:20 -0300

    :lipstick: style: darkmode background modal

--- src/Package/Modal/customStyles.js
+++ src/Package/Modal/customStyles.js
@@ -1,13 +1,19 @@
 export default function customStyles(tema) {
 
     let cor = null
+    let backg = null
     if (tema == 'light') {
         cor = '#ffffff'
+        backg = 'rgb(255, 255, 255, 0.8)'
     } else {
         cor = '#464646'
+        backg = 'rgb(70, 70, 70, 0.8)'
     }
     
     const customStyles = {
+        overlay: {
+            background: backg
+        },
         content: {
             top: '50%',
             left: '50%',
@@ -15,7 +21,8 @@ export default function customStyles(tema) {
             bottom: 'auto',
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
-            backgroundColor: cor
+            borderRadius: '4px',
+            backgroundColor: cor,
         },
     };
 
