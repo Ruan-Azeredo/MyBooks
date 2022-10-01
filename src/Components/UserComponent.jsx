fccc4679bc12bc662c73a88dfb19fd15a7a704d8
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-01 15:37:38 -0300

    :goal_net: fix: erro de hidratação e img user

--- src/Components/UserComponent.jsx
+++ src/Components/UserComponent.jsx
@@ -1,21 +1,29 @@
 import Image from "next/image"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import AuthContext from "../Context/AutenticaçãoContext"
 
 export default function UserComponent() {
-    const { photoglobal, nameglobal } = useContext(AuthContext)
-    const nomeCompleto = 'Ruan Azeredo dos Santos Gomes'
-    const photo = 'https://lh3.googleusercontent.com/a-/AFdZucoglhEEq1usnZ6RKfyWgCP5cd--Xk8UFcEQ7gL9IQ=s96-c'
-    const nome = nomeCompleto.split(" ")[0] //Pega só o primeiro nome
+    const { photoCooked, nameCooked } = useContext(AuthContext)
+
+    //este trecho resolve o problema de Hidratação
+    const [img, setImg] = useState()
+    const [name, setName] = useState()
+    useEffect(() => {
+        setImg(photoCooked)
+        setName(nameCooked)
+    })
+
+    const n = name?.split(" ")[0] //Pega só o primeiro nome
+
     return (
         <div className={`
         w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px] sticky top-11 grid 
         bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100
         dark:bg-gradient-to-br dark:from-escuroDark dark:via-anilDark dark:to-escuroDark  
         `}>
-            <img alt="" src={photoglobal} className={`h-20 w-20 rounded-3xl mx-auto mt-8`} />
+            <img alt="" src={img} className={`h-20 w-20 rounded-3xl mx-auto mt-8`} />
             <div className={`mx-10 px-2`}>Bem vindo de volta</div>
-            <div className={`font-bold justify-self-center text-xl pb-4`}>{ nome }</div>
+            <div className={`font-bold justify-self-center text-xl pb-4`}>{ n }</div>
         </div>
     )
 }
