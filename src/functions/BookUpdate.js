f874eb9a69df3565f5f9cf2374b9418370919615
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-01 15:56:15 -0300

    :construction: build: adiciona idCooked

--- src/functions/BookUpdate.js
+++ src/functions/BookUpdate.js
@@ -1,18 +1,13 @@
-import { useContext } from 'react'
-import AuthContext from '../Context/AutenticaçãoContext'
-
 const axios = require('axios')
 
-export default function BookUpdate(id, titlevalue, writervalue, covervalue, createdAt) {
+export default function BookUpdate(idCooked, id, titlevalue, writervalue, covervalue, createdAt) {
 
-    // const {iduserglobal} = useContext(AuthContext)
-    const iduserglobal = 2
     const deleta = (id) => {
         axios.delete(`http://localhost:3001/users/books/1/1/${id}`)
     }
     console.log('writervalue:', writervalue)
     const posta = (titlevalue, writervalue, covervalue) => {
-        axios.post(`http://localhost:3001/users/books/${iduserglobal}/${writervalue}/`, {
+        axios.post(`http://localhost:3001/users/books/${idCooked}/${writervalue}/`, {
             title: titlevalue,
             cover: covervalue,
             createdAt: createdAt
