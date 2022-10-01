a30a351303d872dad1557ab6af072967c62f7ffc
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-09-30 22:42:32 -0300

    :recycle: refactor: transforma const em function

--- src/Package/Modal/customStyles.js
+++ src/Package/Modal/customStyles.js
@@ -1,10 +1,23 @@
-export const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
-};
+export default function customStyles(tema) {
+
+    let cor = null
+    if (tema == 'light') {
+        cor = '#ffffff'
+    } else {
+        cor = '#464646'
+    }
+    
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: cor
+        },
+    };
+
+    return { customStyles }
+}
