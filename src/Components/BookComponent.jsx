f877d24a9c3fd4de0ae29a0ebaaa4b0e78c4bb6b
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-04 22:35:13 -0300

    :recycle: refactor: muda referencia de cover p url

--- src/Components/BookComponent.jsx
+++ src/Components/BookComponent.jsx
@@ -10,13 +10,13 @@ export default function BookComponent(props) {
     const resp = props.resp
     const escritorTratado = TrataWriter(resp.writer_id, writersglobal)
 
-    const infos = {id: resp.id, title: resp.title, cover: resp.cover, writer: escritorTratado, writer_id: resp.writer_id, createdAt: resp.createdAt}
-
+    const infos = {id: resp.id, title: resp.title, url: resp.url, writer: escritorTratado, writer_id: resp.writer_id, createdAt: resp.createdAt}
+    console.log('resp:',resp)
     return (
         <div className={`mb-4`}>
             <div className={`absolute pt-24 px-8 -z-20`}>+ Resenha</div>
             <button>
-                <img alt='' layout='fill' className={`h-60 w-40 rounded-3xl mb-4 hover:opacity-20 z-30`} src={resp.cover}/>
+                <img alt='' layout='fill' className={`h-60 w-40 rounded-3xl mb-4 hover:opacity-20 z-30`} src={'http://localhost:3001/' + resp.url}/>
             </button>
             <div className={`h-[90px]`}>
                 <div className={`font-semibold text-base w-[180px] mb-2`}>{limitText(resp.title, 40)}</div>
