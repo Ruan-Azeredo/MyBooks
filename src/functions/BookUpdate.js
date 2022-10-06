e68ade18ef3e73489edd226f2d281556a7bac8a4
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-06 10:38:52 -0300

    :fire: chore: retira console.log()

--- src/functions/BookUpdate.js
+++ src/functions/BookUpdate.js
@@ -4,12 +4,8 @@ import BookDelete from './BookDelete'
 const axios = require('axios')
 
 const BookUpdate = (idCooked, writer_id_original, writer_id_novo, id, title_original, title_novo, file, file_name, file_url) => {
-    console.log('file:', file)
-    console.log('w original:', writer_id_original, 'w novo:', writer_id_novo)
-    console.log('title novo', title_novo)
 
     if (writer_id_novo == writer_id_original || writer_id_novo == undefined) {
-        console.log('foi para put')
         
         axios.put(`http://localhost:3001/users/books/${idCooked}/${writer_id_original}/${id}/${title_novo}/${file_name}`, file, {
             headers: {
