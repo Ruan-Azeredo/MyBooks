6cb9b6bb69dc7c061d540fc46455cf82e9e9865f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-06 10:35:24 -0300

    :bug: fix: muda condiciona do button

--- src/Components/ButtonDarkModeComponent.jsx
+++ src/Components/ButtonDarkModeComponent.jsx
@@ -6,8 +6,8 @@ export default function ButtonDarkModeComponent() {
     const {tema, alterarTema} = useContext(DarkModeContext)
 
     return (
-        <div className={`ml-20`}>
-            {tema == 'light' ? (
+        <div className={`ml-2`}>
+            {tema != 'dark' ? (
                 <button className={`bg-gray-600 rounded-full w-[80px] h-7 flex flex-row pt-[3px]`} onClick={alterarTema}>
                 <div className={`ml-1 w-[22px] h-[22px] bg-white rounded-full`}></div>
                 <div className={`ml-[5px] text-white`}>Dark</div>
