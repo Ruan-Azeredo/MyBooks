781b81ef87f2412c267d6f780b796cc831114303
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-06 22:42:08 -0300

    :goal_net: fix: completa src da img

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -14,7 +14,7 @@ export default function ReviewsComponentIndex(props) {
 
     return (
         <div className={`bg-mainColor dark:bg-mainDark h-32 w-96 mr-16 relative rounded-3xl`}>
-            <img alt="" layout="fill" className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={TrataBook(resp.book_id)}/>
+            <img alt="" layout="fill" className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={'http://localhost:3001/' + TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
             <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp.text, 80)}</div>
         </div>
