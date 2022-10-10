19bf6b47a6f36d7c6aed96fd287152338cc8a7a8
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-10 10:09:41 -0300

    :sparkles: feat: adiciona SeeMoreComponent e condi

--- src/content/ReviewContentIndex.jsx
+++ src/content/ReviewContentIndex.jsx
@@ -1,7 +1,15 @@
+import SeeMoreComponent from "../components/SeeMoreComponent";
+
 export default function ReviewContentIndex(props) {
     return (
-        <div className="flex flex-row mt-28 col-start-2 col-end-7">           
-            {props.children}
+        <div className="flex flex-row mt-28 col-start-2 col-end-7">
+            <div className="grid grid-cols-2 gap-16">           
+                {props.children}
+            </div>{
+                props.index == true ? (
+                    <SeeMoreComponent/>
+                ) : null
+            }
         </div>
     )
 }
