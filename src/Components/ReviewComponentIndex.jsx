7d419f9d6fb3bdeb92ecd4afc58aea8c783e69ec
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-10-20 23:45:28 -0300

    :sparkles: feat: adiciona ReviewModalComponent

--- src/Components/ReviewComponentIndex.jsx
+++ src/Components/ReviewComponentIndex.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import limitText from "../functions/limitText"
 import TrataBook from "../functions/TrataBook"
+import ReviewModalComponent from "./ReviewModalComponent"
 
 export default function ReviewsComponentIndex(props) {
     const resp = props.resp
@@ -13,10 +14,16 @@ export default function ReviewsComponentIndex(props) {
     }
 
     return (
-        <div className={`bg-mainColor dark:bg-mainDark h-32 w-96 mr-16 relative rounded-3xl`}>
+        <div className={`bg-mainColor dark:bg-mainDark h-32 w-96 mb-6 relative rounded-3xl`}>
             <img alt="" layout="fill" className={`h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4`} src={'http://localhost:3001/' + TrataBook(resp.book_id)}/>
             <div className={`left-[150px] relative pt-2`}>{transformDate(resp)}</div>
             <div className={`left-[150px] relative pt-2 w-56`}>{limitText(resp.text, 80)}</div>
+            {props.index == false ? (
+                <div>
+                    <ReviewModalComponent />
+                    <ReviewModalComponent />
+                </div>
+            ) : null}
         </div>
     )
 }
