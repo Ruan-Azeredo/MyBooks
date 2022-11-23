e0c68f9de4fb7f189185de39714d69be7dd960ac
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-23 14:02:17 -0300

    :recycle: refactor: style button +resenha

--- src/Components/BookComponent.jsx
+++ src/Components/BookComponent.jsx
@@ -3,6 +3,7 @@ import WritersContext from '../Context/WritersContext'
 import limitText from '../functions/limitText'
 import TrataWriter from '../functions/TrataWriter'
 import BookModalComponent from './BookModalComponent'
+import { IconPencil } from './icons'
 
 export default function BookComponent(props) {
 
@@ -10,13 +11,15 @@ export default function BookComponent(props) {
     const resp = props.resp
     const escritorTratado = TrataWriter(resp.writer_id, writersglobal)
 
-    const infos = {id: resp.id, title: resp.title, name: resp.name, url: resp.url, writer: escritorTratado, writer_id: resp.writer_id, createdAt: resp.createdAt}
+    const infos = { id: resp.id, title: resp.title, name: resp.name, url: resp.url, writer: escritorTratado, writer_id: resp.writer_id, createdAt: resp.createdAt }
 
     return (
         <div className={`mb-4`}>
-            <div className={`absolute pt-24 px-8 -z-20`}>+ Resenha</div>
             <button>
-                <img alt='' layout='fill' className={`h-60 w-40 rounded-3xl mb-4 hover:opacity-20 z-30`} src={'http://localhost:3001/' + resp.url}/>
+                <div className='absolute -mt-3 ml-28 rounded-md bg-anilDark pt-4 pb-2 px-1 text-white'>
+                    <div className={`-rotate-12`}>{IconPencil}</div>
+                </div>
+                <img alt='' layout='fill' className={`h-60 w-40 rounded-3xl mb-4`} src={'http://localhost:3001/' + resp.url}/>
             </button>
             <div className={`h-[90px]`}>
                 <div className={`font-semibold text-base w-[180px] mb-2`}>{limitText(resp.title, 40)}</div>
