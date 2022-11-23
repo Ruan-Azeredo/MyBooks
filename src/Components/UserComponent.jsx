371f5b7aecf5bbd7e06e0611aac1ab6598c28142
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-23 11:49:57 -0300

    :sparkles: fix: conserta UserComponent position

--- src/Components/UserComponent.jsx
+++ src/Components/UserComponent.jsx
@@ -17,11 +17,11 @@ export default function UserComponent() {
 
     return (
         <div className={`
-        w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px] sticky top-11 grid 
+        w-[180px] h-60  col-start-1 col-end-2 rounded-r-[40px] fixed top-11 grid 
         bg-gradient-to-br from-gray-100 via-amber-100 to-gray-100
         dark:bg-gradient-to-br dark:from-escuroDark dark:via-anilDark dark:to-escuroDark  
         `}>
-            <img alt="" src={img} referrerpolicy="no-referrer" className={`h-20 w-20 rounded-3xl mx-auto mt-8 object-cover`} />
+            <img alt="" src={img} referrerPolicy="no-referrer" className={`h-20 w-20 rounded-3xl mx-auto mt-8 object-cover`} />
             <div className={`mx-10 px-2`}>Bem vindo de volta</div>
             <div className={`font-bold justify-self-center text-xl pb-4`}>{ n }</div>
         </div>
