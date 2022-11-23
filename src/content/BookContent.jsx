0b15d885e43ab57e03fef4512f6bc7b52596411e
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-23 11:48:23 -0300

    :fire: feat: reitra mt-t de BookContent

--- src/content/BookContent.jsx
+++ src/content/BookContent.jsx
@@ -1,6 +1,6 @@
 export default function BookContent(props) {
     return (
-        <div className={`w-full flex col-start-2 col-end-6 mt-6`}>
+        <div className={`w-full flex col-start-2 col-end-6`}>
             <div className={`w-full text-xl font-bold`}>Livros
             <div className={`
                 grid grid-cols-4 w-full mt-4 font-medium
