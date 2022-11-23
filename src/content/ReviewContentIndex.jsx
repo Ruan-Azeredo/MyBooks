bf2ad4ba384fec3a8162803807ea05ead3e342e0
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-23 11:47:37 -0300

    :recycle: refactor: ReviewContentIndex conditional

--- src/content/ReviewContentIndex.jsx
+++ src/content/ReviewContentIndex.jsx
@@ -2,14 +2,19 @@ import SeeMoreComponent from "../components/SeeMoreComponent";
 
 export default function ReviewContentIndex(props) {
     return (
-        <div className="flex flex-row mt-28 col-start-2 col-end-7">
-            <div className="grid grid-cols-2 gap-16">           
-                {props.children}
-            </div>{
-                props.index == true ? (
-                    <SeeMoreComponent/>
-                ) : null
-            }
+        <div className="flex flex-col mt-20 col-start-2 col-end-7">
+            {props.index != true ? (
+                <div className={`mb-24 text-xl font-bold`}>Resenhas</div>
+            ) : null}
+            <div className="flex flex-row">
+                <div className="grid grid-cols-2 gap-16">           
+                    {props.children}
+                </div>{
+                    props.index == true ? (
+                        <SeeMoreComponent/>
+                    ) : null
+                }
+            </div>
         </div>
     )
 }
