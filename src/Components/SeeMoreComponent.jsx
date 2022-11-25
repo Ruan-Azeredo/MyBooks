11c80f9897856d6fe23d9e6db7935258654506b0
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-24 23:56:13 -0300

    :constrcution: build: modal add reviews

--- src/Components/SeeMoreComponent.jsx
+++ src/Components/SeeMoreComponent.jsx
@@ -1,13 +1,34 @@
+import { useContext, useState } from "react";
+import Modal from "react-modal";
+import DarkModeContext from "../Context/DarkModeContext";
+import customStyles from "../Package/Modal/customStyles";
+
 export default function SeeMoreComponent() {
+
+    const {tema} = useContext(DarkModeContext)
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+    // const [data, setData] = useState({})
+    const openModal = () => {
+    setIsOpen(true);
+    }
+    const closeModal = () => {
+    setIsOpen(false)
+    }
+    const customStylesM = customStyles(tema).customStyles
+    
     return (
         <div>
-            <div className={`bg-yellow-100 ml-16 dark:bg-slate-700 h-32 w-56 relative rounded-3xl`}>
+            <button onClick={openModal} className={`bg-yellow-100 ml-16 dark:bg-slate-700 h-32 w-56 relative rounded-3xl pointer`}>
                 <div className={`
                 h-[180px] w-[120px] rounded-3xl absolute -top-16 left-4 bg-yellow-100 dark:bg-slate-700
                 `}></div>
                 <div className={`pt-28 pl-8 absolute -top-16 left-7 font-bold text-xl`}>+ Resenhas</div>
-            </div>
+            </button>
 
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStylesM}>
+                <div></div>
+            </Modal>
         </div>
     )
 }
