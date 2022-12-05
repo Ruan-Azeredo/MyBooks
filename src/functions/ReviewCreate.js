5eda8accd131a4487f56690f180d42e636eed868
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-05 11:25:22 -0300

    :bug: fix; conserta route API

--- src/functions/ReviewCreate.js
+++ src/functions/ReviewCreate.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const ReviewCreate = (idCooked, book_id, text) => {
-    axios.post(`http://localhost:3001/users/reviews/user_id/writer_id/${idCooked}/${book_id}`, {
+    axios.post(`http://localhost:3001/users/reviews/${idCooked}/writer_id/${book_id}`, {
         text: text
     })
 }
