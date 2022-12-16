ce43f5d6bd56a17bc0516c4183f0fdf809086e7a
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-15 22:59:03 -0300

    :construction: build: chamada e passagem de props

--- src/Components/EditWritersComponent.jsx
+++ src/Components/EditWritersComponent.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import WriterModalComponent from "./WriterModalComponent";
+import AuthContext from '../Context/AutenticaçãoContext'
 
 export default function EditWritersComponent(props) {
+    const { idCooked } = useContext(AuthContext)
     
     return (
         <div className="bg-gradient-to-br from-neutral-200 via-neutral-50 to-neutral-50
@@ -8,8 +11,8 @@ export default function EditWritersComponent(props) {
         sombra rounded-3xl my-5 mr-5 p-3 flex flex-col justify-between h-28">
             <div className="mx-2 text-lg leading-6">{props.resp.name}</div>
             <div className="flex justify-between">
-                <WriterModalComponent value='Update' />
-                <WriterModalComponent value='Delete'/>
+                <WriterModalComponent value='Update' idCooked={idCooked} resp={props.resp} />
+                <WriterModalComponent value='Delete' idCooked={idCooked} resp={props.resp}/>
             </div>
         </div>
     )
