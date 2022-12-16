629fe17dac99499f6e82833295e0e8da8a0a5e17
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-15 22:58:34 -0300

    :construction: build: update writers modal

--- src/Components/WriterModalComponent.jsx
+++ src/Components/WriterModalComponent.jsx
@@ -2,10 +2,15 @@ import customStyles from "../Package/Modal/customStyles";
 import Modal from "react-modal";
 import { useContext, useState } from "react";
 import DarkModeContext from "../Context/DarkModeContext";
+import WriterUpdate from "../functions/WriterUpdate";
+import WriterDelete from "../functions/WriterDelete";
 
 export default function WriterModalComponent(props) {
 
     const { tema } = useContext(DarkModeContext)
+
+    const [autor, setAutor] = useState()
+
     const [modalIsOpen, setIsOpen] = useState(false);
     const openModal = () => {
         setIsOpen(true);
@@ -15,13 +20,27 @@ export default function WriterModalComponent(props) {
     }
     const customStylesM = customStyles(tema).customStyles
 
+    const getAutor = () => setAutor(event.target.value)
+
     let modalConteudo = null
     let borderColor = null
 
     if (props.value == 'Update') {
         borderColor = 'border-mainColor dark:border-mainDark'
         modalConteudo = (
-            <div></div>
+            <div className={`mx-5 ${tema}`}>
+                <div className={`w-full flex flex-row pb-5 dark:text-white`}>
+                    <div className="w-full">ATUALIZAR AUTOR</div>
+                    <button onClick={closeModal} className={`flex justify-end w-full`}>[x]</button>
+                </div>
+                <form>
+                    <div className={`mr-5 dark:text-white`}>
+                            <label>Autor</label>
+                            <input type="text" defaultValue={props.resp.name} onChange={getAutor} required className={`flex bg-mainColor dark:bg-mainDark rounded-md w-72 h-10 pl-2 mb-10 `} />
+                    </div>
+                    <button className={`dark:text-white bg-mainColor dark:bg-mainDark px-4 py-2 rounded-full`} onClick={()=>WriterUpdate(props.idCooked, props.resp.id, autor)}>Atualizar Autor</button>
+                </form>
+            </div>
         )
 
     } else {
