4b774093d0405bccfaec043da13c539f23f000c2
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-15 22:35:23 -0300

    :recycle: refactor: reorganiza colunas dif views

--- src/content/ReviewContentIndex.jsx
+++ src/content/ReviewContentIndex.jsx
@@ -1,10 +1,11 @@
 import SeeMoreComponent from "../components/SeeMoreComponent";
 
 export default function ReviewContentIndex(props) {
+    const col = props.index != true ? '6' : '7'
     return (
-        <div className="flex flex-col mt-20 col-start-2 col-end-7">
+        <div className={`flex flex-col mt-20  z-[0] col-start-2 col-end-${col}`}>
             {props.index != true ? (
-                <div className={`mb-24 text-xl font-bold`}>Resenhas</div>
+                <div className={`mb-20 text-xl font-bold`}>Resenhas</div>
             ) : null}
             <div className="flex flex-row">
                 <div className="grid grid-cols-2 gap-16 pt-10">           
